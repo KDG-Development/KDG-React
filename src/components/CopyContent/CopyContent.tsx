461bbfe32d4510149d4ba33c1b800f248c817ef2
Brandyn Lordi <blordi@kyledavidgroup.com>
2024-10-10 15:03:50 -0400

    added copycontent component

---
+++ src/components/CopyContent/CopyContent.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Clickable from '../Clickable'
+import { Conditional } from '../Conditional'
+import Icon from '../Icon'
+
+type TCopyContentProps = {
+  content:string
+  className?: string
+}
+
+export const CopyContent = (props:React.PropsWithChildren<TCopyContentProps>) => {
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(props.content)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 2000)
+  }
+
+  return (
+    <>
+      {props.content}
+      <Conditional
+        condition={copied}
+        onTrue={() => <Icon icon={x => x.cilCheck} />}
+        onFalse={() =>
+          <Clickable onClick={handleCopy} className={props.className}>
+            <Icon icon={x => x.cilCopy} />
+          </Clickable>
+        }
+      />
+    </>
+  )
+}
